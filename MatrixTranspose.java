77b0d33df1d0b6f8eb321fd5e57b21f5ff3927d6
ayushkrishnan <114354841+ayushkrishnan@users.noreply.github.com>
2022-09-27 16:27:36 +0530

    Add files via upload

--- MatrixTranspose.java
+++ MatrixTranspose.java
@@ -19,7 +19,7 @@ public class MatrixTranspose
 		row1=in.nextInt();
 		column1=in.nextInt();		
 		matrix1=new int[row1][column1];				
-		matrix2=new int[column1][row1];
+		//matrix2=new int[column1][row1];
 		System.out.println("Enter the Elements of the  Matrix1 ");
 		for(int i=0;i<row1;i++)
 		{
@@ -35,26 +35,15 @@ public class MatrixTranspose
 			{
 				System.out.print(matrix1[i][j]+"\t");
 			}System.out.println();
-		}
-		
+		}						
+		System.out.println("The elements of Transpose Matrix is ");
 		for(int i=0;i<column1;i++)
 		{
 			for(int j=0;j<row1;j++)
 			{
-				matrix2[i][j]=matrix1[j][i];
-			}
-		}		
-			
-			
-			
-			System.out.println("The elements of Transpose Matrix is ");
-			for(int i=0;i<column1;i++)
-			{
-				for(int j=0;j<row1;j++)
-				{
-					System.out.print(matrix2[j][i]+"\t");
-				}System.out.println();
-			}
+				System.out.print(matrix1[j][i]+"\t");
+			}System.out.println();
+		}
 		
 
 	}
